1200a01ee0498384f7f2580fc9358e9ae0f2f97a
ratnani1996 <himanshuratnani1996@gmail.com>
2019-02-13 18:26:52 +0530

    21% completed

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,5 +1,4 @@
 
-
 $("document").ready(()=>{
     $(".btn-success").on('click', (e)=>{
         var inputTime = $("#time").val();
@@ -7,10 +6,16 @@ $("document").ready(()=>{
         DrawLocks(inputLocks);
         SolvePuzzle(inputTime, inputLocks);
     })
+    
+    
+    $(".btn-danger").on('click', (e)=>{
+        $("#lock_area").empty();
+    })
 })
 
 
 var DrawLocks = (inputLocks) => {
+    inputLocks = parseInt(inputLocks);
     if( !isNaN(inputLocks) ){
         while( inputLocks-- ){
             $("#lock_area").append(`<i class="fas fa-lock fa-4x"></i>`)
@@ -22,13 +27,24 @@ var DrawLocks = (inputLocks) => {
 }
 
 var SolvePuzzle = (inputTime, inputLocks) =>{
+    inputTime = parseInt(inputTime);
+    inputLocks = parseInt(inputLocks);
     var counter = 1;
-    setInterval( ()=>{
-        while(counter<inputLocks){
-            for(var incremental = counter; incremental <= inputLocks; incremental += counter){
-                $(".fa-lock .fa-unlock").toggleClass('fa-lock', 'fa-unlock');
-                counter++;
-            }
+    
+    while(counter<inputLocks){
+        for(var incremental = counter; incremental <= inputLocks; incremental += counter){
+            $(".fas").toggleClass(`fa-lock`, `fa-unlock`);
         }
-    } , inputTime)
+        counter+=1;
+    }
+    
+//    setInterval(
+//    (()=>{
+//        while(counter<inputLocks){
+//            for(var incremental = counter; incremental <= inputLocks; incremental += counter){
+//                $(".fa-lock .fa-unlock").toggleClass('fa-lock', 'fa-unlock');
+//                counter++;
+//            }
+//        })()
+//    } , inputTime)
 }
